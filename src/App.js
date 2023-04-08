233c35ad06e40fe84aade12d4987073670b6c150
srehan1 <syed.rehan@shoppertrak.com>
2023-04-08 18:18:04 +0530

    Linking deployed contract with react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react";
+import lottery from "./lottery";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { manager: "", players: "" };
+  }
+  async componentDidMount() {
+    // await window.ethereum.request({ method: "eth_requestAccounts" });
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayerList().call();
+    this.setState({ manager, players: players.length });
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <h1>This is our Lottery game.</h1>
+        <p>Contract is managed by {this.state.manager}</p>
+        <h3>Number of players joined: {this.state.players}</h3>
       </div>
     );
   }
---
+++ src/lottery.js
@@ -0,0 +1,59 @@
+import web3 from "./web3";
+
+const address = "0x7757a7D1108Dd5d1a420281BAeace71Fbf31BB34";
+const abi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+    signature: "constructor",
+  },
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+    payable: true,
+    signature: "0xe97dcb62",
+  },
+  {
+    inputs: [],
+    name: "getPlayerList",
+    outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+    signature: "0x62a63e29",
+  },
+  {
+    inputs: [],
+    name: "manager",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+    signature: "0x481c6a75",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+    payable: true,
+    signature: "0x5d495aea",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "players",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+    constant: true,
+    signature: "0xf71d96cb",
+  },
+];
+
+const contract = new web3.eth.Contract(abi, address);
+export default contract;
